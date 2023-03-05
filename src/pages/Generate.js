a79c454be6ea7295826d2407166557a52eaefddf
RSiby <86668225+RivaldiSiby@users.noreply.github.com>
2023-03-05 18:54:35 +0800

    fix generate

--- src/pages/Generate.js
+++ src/pages/Generate.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { exportComponentAsJPEG } from "react-component-export-image";
 import { useLocation, useNavigate } from "react-router-dom";
 import Print from "../components/print/Print";
@@ -7,25 +7,48 @@ function Generate() {
   const navigate = useNavigate();
   const { state } = location;
   const componentRef = useRef();
+  const [timer, setTimer] = useState(5);
+  const [hide, SetHide] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer - 1);
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(interval);
+      SetHide(false);
+    }, 5000);
+  }, [timer]);
 
   return (
     <React.Fragment>
-      <Print data={state.data} ref={componentRef} />
+      <div className="mx-auto">
+        <Print data={state.data} ref={componentRef} />
+      </div>
       <div className="box-generate">
-        <button
-          className="btn btn-light btn-cos"
-          onClick={() =>
-            exportComponentAsJPEG(componentRef, { fileName: "Laporan Kerja" })
-          }
-        >
-          Download To Img
-        </button>
-        <button
-          onClick={() => navigate("/", { replace: true })}
-          className="btn btn-light btn-cos"
-        >
-          Back
-        </button>
+        {hide ? (
+          <p>Silahkan tunggu {timer}</p>
+        ) : (
+          <>
+            <button
+              className="btn btn-light btn-cos"
+              onClick={() => {
+                exportComponentAsJPEG(componentRef, {
+                  fileName: "Laporan Kerja",
+                });
+                navigate("/", { replace: true });
+              }}
+            >
+              Download
+            </button>
+            <button
+              onClick={() => navigate("/", { replace: true })}
+              className="btn btn-light btn-cos"
+            >
+              Back
+            </button>
+          </>
+        )}
       </div>
     </React.Fragment>
   );
